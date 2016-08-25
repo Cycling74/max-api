942714d255e96e970f7ed5e553a990fc5a34ddd2
Timothy Place <tim@cycling74.com>
2016-08-25 06:54:46 -0500

    typo fix

--- script/max-pretarget.cmake
+++ script/max-pretarget.cmake
@@ -16,7 +16,7 @@ if (WIN32)
     set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
     set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
     set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
-end ()
+endif ()
 
 project(${THIS_FOLDER_NAME})
 
