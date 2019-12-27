c5e4ac0e1917c99b874b3a53552a8cda43687cfd
Timothy Place <tim@cycling74.com>
2019-12-27 08:37:43 -0700

    whitespace: tabs->spaces and trim trailing whitespace

--- include/c74_max_preprocessor.h
+++ include/c74_max_preprocessor.h
@@ -1,24 +1,24 @@
 /*
-	Copyright (c) 2011, Stefan Reinalter
+    Copyright (c) 2011, Stefan Reinalter
 
-	This software is provided 'as-is', without any express or implied
-	warranty. In no event will the authors be held liable for any damages
-	arising from the use of this software.
+    This software is provided 'as-is', without any express or implied
+    warranty. In no event will the authors be held liable for any damages
+    arising from the use of this software.
 
-	Permission is granted to anyone to use this software for any purpose,
-	including commercial applications, and to alter it and redistribute it
-	freely, subject to the following restrictions:
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
 
-	1. The origin of this software must not be misrepresented; you must not
-	claim that you wrote the original software. If you use this software
-	in a product, an acknowledgment in the product documentation would be
-	appreciated but is not required.
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
 
-	2. Altered source versions must be plainly marked as such, and must not be
-	misrepresented as being the original software.
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
 
-	3. This notice may not be removed or altered from any source
-	distribution.
+    3. This notice may not be removed or altered from any source
+    distribution.
 */
 
 // ext_preprocessor.h:
@@ -85,17 +85,17 @@
 
 /// Returns 1 if the argument list to the variadic macro is empty, 0 otherwise.
 #define C74_IS_EMPTY(...)														\
-	C74_HAS_COMMA																\
-	(																			\
-		C74_JOIN_5                                                              \
-		(																		\
-			C74_IS_EMPTY_CASE_,                                                 \
-			C74_HAS_COMMA(__VA_ARGS__),                                         \
-			C74_HAS_COMMA(C74_IS_EMPTY_BRACKET_TEST __VA_ARGS__),               \
-			C74_HAS_COMMA(__VA_ARGS__ (~)),                                     \
-			C74_HAS_COMMA(C74_IS_EMPTY_BRACKET_TEST __VA_ARGS__ (~))            \
-		)																		\
-	)
+    C74_HAS_COMMA																\
+    (																			\
+        C74_JOIN_5                                                              \
+        (																		\
+            C74_IS_EMPTY_CASE_,                                                 \
+            C74_HAS_COMMA(__VA_ARGS__),                                         \
+            C74_HAS_COMMA(C74_IS_EMPTY_BRACKET_TEST __VA_ARGS__),               \
+            C74_HAS_COMMA(__VA_ARGS__ (~)),                                     \
+            C74_HAS_COMMA(C74_IS_EMPTY_BRACKET_TEST __VA_ARGS__ (~))            \
+        )																		\
+    )
 
 #define C74_IS_EMPTY_CASE_0001			,
 #define C74_IS_EMPTY_BRACKET_TEST(...)	,
