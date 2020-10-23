4b6b7d9f2be724b2b71397105b30cfdab9e1d9eb
Hidde de Jong <hiddedejong0@gmail.com>
2020-10-23 22:06:20 +0200

    Cleanup: spacing

--- script/max-pretarget.cmake
+++ script/max-pretarget.cmake
@@ -3,7 +3,7 @@
 
 string(REGEX REPLACE "(.*)/" "" THIS_FOLDER_NAME "${CMAKE_CURRENT_SOURCE_DIR}")
 
-if (WIN32)
+if(WIN32)
 	# These must be prior to the "project" command
 	# https://stackoverflow.com/questions/14172856/compile-with-mt-instead-of-md-using-cmake
 
@@ -16,7 +16,7 @@ if (WIN32)
     set(CMAKE_CXX_FLAGS_MINSIZEREL     "/MT /O1 /Ob1 /D NDEBUG")
     set(CMAKE_CXX_FLAGS_RELEASE        "/MT /O2 /Ob2 /D NDEBUG")
     set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/MT /Zi /O2 /Ob1 /D NDEBUG")
-endif ()
+endif()
 
 project(${THIS_FOLDER_NAME})
 
@@ -26,34 +26,34 @@ set(C74_SCRIPTS "${C74_MAX_API_DIR}/script")
 
 set(C74_CXX_STANDARD 0)
 
-if (APPLE)
-	if (CMAKE_OSX_ARCHITECTURES STREQUAL "")
+if(APPLE)
+	if(CMAKE_OSX_ARCHITECTURES STREQUAL "")
 		set(CMAKE_OSX_ARCHITECTURES x86_64)
 	endif()
-endif ()
+endif()
 
-if (NOT DEFINED C74_BUILD_MAX_EXTENSION)
+if(NOT DEFINED C74_BUILD_MAX_EXTENSION)
 	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../../../externals")
-else ()
+else()
 	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../../../extensions")
-endif ()
+endif()
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
 
-if (WIN32)
+if(WIN32)
 
 	set(CMAKE_COMPILE_PDB_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/tmp")
 	set(CMAKE_PDB_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/tmp")
 
-	if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 		SET(MaxAPI_LIB ${C74_MAX_API_DIR}/lib/win64/MaxAPI.lib)
 		SET(MaxAudio_LIB ${C74_MAX_API_DIR}/lib/win64/MaxAudio.lib)
 		SET(Jitter_LIB ${C74_MAX_API_DIR}/lib/win64/jitlib.lib)	
-	else ()
+	else()
 		SET(MaxAPI_LIB ${C74_MAX_API_DIR}/lib/win32/MaxAPI.lib)
 		SET(MaxAudio_LIB ${C74_MAX_API_DIR}/lib/win32/MaxAudio.lib)
 		SET(Jitter_LIB ${C74_MAX_API_DIR}/lib/win32/jitlib.lib)
-	endif ()
+	endif()
 
 	MARK_AS_ADVANCED (MaxAPI_LIB)
 	MARK_AS_ADVANCED (MaxAudio_LIB)
@@ -64,11 +64,11 @@ if (WIN32)
 		-DWIN_VERSION
 		-D_USE_MATH_DEFINES
 	)
-else ()
+else()
 	file (STRINGS "${C74_MAX_API_DIR}/script/max-linker-flags.txt" C74_SYM_MAX_LINKER_FLAGS)
 	file (STRINGS "${C74_MAX_API_DIR}/script/msp-linker-flags.txt" C74_SYM_MSP_LINKER_FLAGS)
 
 	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${C74_SYM_MAX_LINKER_FLAGS} ${C74_SYM_MSP_LINKER_FLAGS}")
 	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${C74_SYM_MAX_LINKER_FLAGS} ${C74_SYM_MSP_LINKER_FLAGS}")
 	set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${C74_SYM_MAX_LINKER_FLAGS} ${C74_SYM_MSP_LINKER_FLAGS}")
-endif ()
+endif()
